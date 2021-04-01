bb971a9d7c45e2880a45a6d41ab6dabdc10f034a
Yurii Kosiv <41447717+yurdosii@users.noreply.github.com>
2021-04-01 21:36:19 +0300

    Feature/5 /test_email_campaign endpoint

--- utils/emails.py
+++ utils/emails.py
@@ -1,15 +1,16 @@
 from django.conf import settings
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives
 
 # TODO - html response in email (щоб гарно було, може якусь табличку)
 
 
 def send_email_message(subject, body, to):
-    email = EmailMessage(
+    email = EmailMultiAlternatives(
         subject=subject,
         body=body,
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=to,
     )
+    email.content_subtype = "html"  # Main content is now text/html
 
     email.send()
