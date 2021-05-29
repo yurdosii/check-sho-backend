cd120cb998eb81e62f851948684ec1c88a1f90a0
yurii <yurdosii.ksv@gmail.com>
2021-05-29 18:55:46 +0300

    feat(users): added signal to create User when TelegramUser is created to be able to assign campaigns if customer uses application only from Telegram. Changed /link_telegram logic to sync campaigns between user and telegram_user's user

--- users/helpers.py
+++ users/helpers.py
@@ -6,6 +6,32 @@ from checksho_bot.models import TelegramUser
 from .models import User
 
 
+def link_telegram_user_to_user(user: User, telegram_response: dict):
+    """
+    After login via telegram on client, callback with telegram data is sent
+    Get or create TelegramUser and link it with User (set to User.telegram_user field)
+    Transfer campaigns from TelegramUser's to User
+    Set TelegramUser's user is_active=False
+    """
+    telegram_user = handle_telegram_user(telegram_response)
+    if not telegram_user:
+        logging.warning(
+            "Unable to get or create telegram user on telegram login callback"
+        )
+        return
+
+    telegram_user_user = getattr(telegram_user, "user", None)
+    if telegram_user_user:
+        transfer_campaigns_from_user_to_user(telegram_user_user, user)
+        remove_user(telegram_user_user)
+        remove_telegram_user_from_user(telegram_user_user)
+
+    user.telegram_user = telegram_user
+    user.save()
+
+    return user
+
+
 def handle_telegram_user(data: dict):
     telegram_id = data.get("id")
     first_name = data.get("first_name")
@@ -24,25 +50,26 @@ def handle_telegram_user(data: dict):
     return telegram_user
 
 
-def link_telegram_user_to_user(user: User, telegram_response: dict):
-    """
-    After login via telegram on client callback with telegram data is sent
-    Get or create TelegramUser and link it with User (set to User.telegram_user field)
-    """
-    # TODO - sync campaigns and delete that user (set is_active=False (на всякий випадок))
-    telegram_user = handle_telegram_user(telegram_response)
-    if not telegram_user:
-        logging.warning(
-            "Unable to get or create telegram user on telegram login callback"
-        )
-        return
+def transfer_campaigns_from_user_to_user(user_from, user_to):
+    for campaign in user_from.campaigns.all():
+        campaign.owner = user_to
+        campaign.save()
 
-    if not user.telegram_user:
-        # TODO - telegram_user should be FK, not OneToOne
-        user.telegram_user = telegram_user
-        user.save()
 
-    return user
+def remove_user(user):
+    user.is_active = False
+    user.save()
+
+
+def remove_telegram_user_from_user(user):
+    """
+    Since OneToOne is declared in User
+    It only works when User.telegram_user = None
+    TelegramUser.user = None - doesn't work
+    """
+    user.telegram_user = None
+    user.save()
+
 
 
 def set_user_role_on_creation(user):
@@ -53,3 +80,24 @@ def set_user_role_on_creation(user):
     if not user.role:
         user.role = User.USER
         user.save()
+
+
+def create_user_for_telegram_user(telegram_user):
+    """
+    If user use application just from Telegram, he should have user
+    so we can assign campaigns to him
+
+    When TelegramUser will be linked with user, this created user will be deleted
+    and campaigns will sync
+    """
+    now = timezone.now()
+    username = f"{telegram_user.first_name} {now.timestamp()}"
+    password = f"{username} password"
+
+    user = User(
+        username=username,
+        password=password,
+        role=User.USER,
+        telegram_user=telegram_user,
+    )
+    user.save()
--- users/signals.py
+++ users/signals.py
@@ -1,7 +1,9 @@
 from django.db.models import signals
 from django.dispatch import receiver
 
-from . import models, helpers
+from checksho_bot.models import TelegramUser
+
+from . import helpers, models
 
 
 @receiver(signals.post_save, sender=models.User)
@@ -9,3 +11,14 @@ def set_role_on_creation(instance, **kwargs):
     created = kwargs.get("created")
     if created:
         helpers.set_user_role_on_creation(instance)
+
+
+@receiver(signals.post_save, sender=TelegramUser)
+def create_user_on_telegram_user_creation(instance, **kwargs):
+    """
+    Don't know how to init signals in checksho_bot (no apps.py file)
+    so to not break anything I'll just call it here
+    """
+    created = kwargs.get("created")
+    if created:
+        helpers.create_user_for_telegram_user(instance)
